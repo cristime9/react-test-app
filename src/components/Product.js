f5419f9bdda5f30f26dfe226537405f48ab208ee
cristime9 <cristianmergea@outlook.com>
2019-07-09 16:23:31 +0300

    Add React App Files
    Add files from Desktop Git APP

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Product NEW</h3>
+      </div>
+    )
+  }
+}
